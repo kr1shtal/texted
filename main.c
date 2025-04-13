16ed780cd18689680def085408930bb2d189f47c
kr1shtal <krishtalforkato@gmail.com>
2025-04-13 14:00:00 +0300

    Colorful search results

--- main.c
+++ main.c
@@ -39,7 +39,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 // data
@@ -245,6 +246,8 @@ int editorSyntaxToColor(int hl) {
     switch (hl) {
         case HL_NUMBER:
             return 31;
+        case HL_MATCH:
+            return 34;
         default:
             return 37;
     }
@@ -547,6 +550,8 @@ void editorFindCallback(char *query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
